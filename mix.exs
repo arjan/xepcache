0c987990aecdfc1f7a4c86ed7614894e4e8c40e0
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-04-20 08:18:59 +0200

    1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Depcache.Mixfile do
 
   def project do
     [app: :xepcache,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
